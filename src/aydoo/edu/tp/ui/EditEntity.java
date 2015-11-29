aadddf8aff47a08b7691f9bd18b10d4b5f413c75
marianocastellano <castellano.mariano3@gmail.com>
2015-11-29 11:20:58 -0300

    Se fixea el error

--- src/aydoo/edu/tp/ui/EditEntity.java
+++ src/aydoo/edu/tp/ui/EditEntity.java
@@ -1,18 +1,9 @@
 package aydoo.edu.tp.ui;
 
-import java.awt.GridLayout;
-import java.util.Iterator;
-
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public class EditEntity {
 
@@ -31,23 +22,27 @@ public class EditEntity {
 	}
 
 	private void drawFields(String content, Box box) {
-		JSONObject jsonObject = new JSONObject(content);
-		JSONArray campos = jsonObject.getJSONArray("campos");
 
-		Iterator<Object> iterator = campos.iterator();
-
-		while (iterator.hasNext()) {
-			JSONObject object = (JSONObject) iterator.next();
-			String name = object.getString("nombre");
-
-			JPanel jPanel = new JPanel(new GridLayout());
-			JLabel nameLabel = new JLabel(name);
-			JTextField nameInput = new JTextField("", 15);
-			jPanel.add(nameLabel);
-			jPanel.add(nameInput);
-
-			box.add(jPanel);
-		}
+		// Pincha al compilar ya que no conoce la libreria.
+		
+		
+		// JSONObject jsonObject = new JSONObject(content);
+		// JSONArray campos = jsonObject.getJSONArray("campos");
+		//
+		// Iterator<Object> iterator = campos.iterator();
+		//
+		// while (iterator.hasNext()) {
+		// JSONObject object = (JSONObject) iterator.next();
+		// String name = object.getString("nombre");
+		//
+		// JPanel jPanel = new JPanel(new GridLayout());
+		// JLabel nameLabel = new JLabel(name);
+		// JTextField nameInput = new JTextField("", 15);
+		// jPanel.add(nameLabel);
+		// jPanel.add(nameInput);
+		//
+		// box.add(jPanel);
+		// }
 
 		JButton button = new JButton("GUARDAR");
 		button.setHorizontalAlignment(SwingConstants.CENTER);
